ac5dddc24249096cd0d2d1e77fd1e555f57ea0db
Ivan Adi Saputra <ivanadisaputra34@gmail.com>
2024-12-08 15:27:24 +0700

    add migration

---
+++ app/Models/OperatorCard.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OperatorCard extends Model
+{
+    protected $table = 'operator_cards';
+
+    protected $guarded = ['id'];
+}
---
+++ app/Models/TransferHistory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TransferHistory extends Model
+{
+    protected $table = 'transfer_histories';
+
+    protected $guarded = ['id'];
+}
